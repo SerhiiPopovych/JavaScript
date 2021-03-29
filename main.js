ac110b8708fe00f45c13dda643f12e1f78812e5b
SerhiiPopovych <psoinua@gmail.com>
2021-03-29 13:01:09 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,227 @@
+// zavdannja 1
+// let hello = 'hello';
+// let owu = 'owu';
+// let com = 'com';
+// let ua = 'ua';
+// let one = 1;
+// let ten = 10;
+// let m999 = -999;
+// let ott = 123;
+// let p = 3.14;
+// let ts = 2.7;
+// let st = 16;
+
+// console.log(hello + name + com + ua);
+// alert(name)
+// document.write(hello+name+com+ua+ten)
+
+// zavdannja 2
+// hello = 'hello ok10';
+// console.log(hello)
+
+// zavdannja 3
+// const a = 27;
+// const b = 186;
+// const c = 100;
+//
+// const name = 'Serhiy';
+// const surname = 'Popovych';
+// const patronymic = 'Olexandrovych';
+
+// zavadannja 4
+// let name1 = prompt('name');
+// let surname1 = prompt('surname');
+// let patronymic1 = prompt('patronymic');
+// console.log(name1);
+// console.log(surname1);
+// console.log(patronymic1);
+
+// zavdannja 5
+// let person = name1 + surname1 + patronymic1;
+// document.write(person);
+
+// zavdannja 6
+
+
+// zavdannja 7
+// let chyslo1 = +prompt('chyslo 1');
+// let chyslo2 = +prompt('chyslo 2');
+// let chyslo3 = +prompt('chyslo 3');
+// console.log(chyslo1);
+// console.log(chyslo2);
+// console.log(chyslo3);
+
+// zavdannja 8
+// let chyslo1 = prompt('chyslo 1');
+// let chyslo2 = prompt('chyslo 2');
+// let chyslo3 = prompt('chyslo 3');
+// let chyslo4 = prompt('chyslo 4');
+//
+// chyslo1 = parseInt(chyslo1);
+// chyslo2 = parseInt(chyslo2);
+// chyslo3 = parseInt(chyslo3);
+// chyslo4 = parseInt(chyslo4);
+//
+// let result = chyslo1+chyslo2+chyslo3+chyslo4;
+// console.log(result);
+
+// zavdannja 9
+// let chyslo1 = prompt('chyslo 1');
+// let chyslo2 = prompt('chyslo 2');
+// let chyslo3 = prompt('chyslo 3');
+//
+// chyslo1 = parseFloat(chyslo1);
+// chyslo2 = parseFloat(chyslo2);
+// chyslo3 = parseFloat(chyslo3);
+//
+// let result = chyslo1 + chyslo2 + chyslo3;
+// console.log(result);
+
+// zavdannja 10
+// let chyslo1 = prompt('chyslo 1');
+// let chyslo2 = prompt('chyslo 2');
+// let chyslo3 = prompt('chyslo 3');
+//
+// chyslo1 = Math.round(chyslo1);
+// chyslo2 = Math.round(chyslo2);
+// chyslo3 = Math.round(chyslo3);
+//  let result = chyslo1 + chyslo2 + chyslo3;
+//  console.log(result)
+
+// zavdannja 11
+// let chyslo1 = prompt('chyslo 1');
+// let chyslo2 = prompt('chyslo 2');
+// let result = Math.pow(chyslo1, chyslo2)
+// console.log(result)
+
+// zavdannja 12
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+//
+// console.log(typeof a, typeof b, typeof c, typeof d)
+
+// zavdannja 13
+// 5 < 6 -> true
+// 5 > 6 -> false
+// 5 >= 6 -> false
+// 5 ? 6 -> false
+// 10 = 10 -> true
+// 10 ? 10 -> true
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 123 = '123' -> false
+// 123 ? '123' -> true
+
+// zavdannja 14
+// console.log(132 > 100 && 45 < 12 );
+// console.log(34 > 33 && 23 < 90 );
+// console.log(99 > 100 && 45 > 12 );
+// console.log(132 > 100 || 45 < 12 );
+// console.log(111 > 11 || 45 < 111 );
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );
+// console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));
+// console.log(!!'-1');
+// console.log(!!-1);
+// console.log(!!'0');
+// console.log(!!'null');
+// console.log(!!'undefined');
+// console.log(!!(3/'owu'));
+// console.log((111 > 11 || 45 < 111) ||  !!'0');
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));
+
+                        // CLASS
+// Class 1
+// let str = 'привет';
+// let num = 123;
+// let flag = true;
+// let txt = 'true'
+// console.log(typeof str, typeof num, typeof flag, typeof txt)
+
+// Class 2
+// let a1 = 5+3;
+// let a2 = 5-3;
+// let a3 = 5*3;
+// let a4 = 5/3;
+// let a5 = 5%3;
+//
+// console.log(a1, a2, a3, a4, a5)
+
+// Class 3
+// let a6 = 5%3;
+// let a7 = 3%5;
+// let a8 = 5+'3';
+// let a9 = '5'-3;
+// let a10 = 75+'kg';
+//
+// console.log(a6, a7, a8, a9, a10)
+
+// Class 4
+// let height = 23;
+// let width = 10;
+// let s = height*width;
+// console.log(s)
+
+// Class 5
+// let heightC = 10;
+// let dC = 4;
+// let v = 3.14 * Math.pow(dC/2, 2) * heightC;
+// console.log(v)
+
+// Class 6
+// let n = 3;
+// let m = 4;
+// let k = Math.sqrt(Math.pow(n,2)+Math.pow(m,2),2)
+// console.log(k)
+
+// Class 7
+// let str = 'Hello world';
+//
+// document.write(str);
+// alert(str);
+// console.log(str);
+
+// Class 8
+// alert('Попович Сергій Олександрович\n 27\n JavaScript')
+
+// Class 9
+// let str1 = 'Кто';
+// let str2 = 'ти';
+// let str3 = 'такой?';
+// let concatenation = str1 + ' ' + str2 + ' ' + str3;
+// document.write(concatenation);
+
+// Class 10
+// let str = '20';
+// let a = 5;
+// document.write(str + a + "<br/>") //205
+// document.write(str - a + "<br/>") //Nan??? непоняв чого 15  а не нан
+// document.write(str * '2' + "<br/>") //Nan???
+// document.write(str / 2 + "<br/>") //Nan???
+
+// Class 11
+// parseInt("3.14") //3
+// parseInt("-7.875") //-7
+// parseInt("435") //435
+// parseInt("Вася") //Nan
+//
+// console.log(parseInt("3.14"), parseInt("-7.875"), parseInt("435"), parseInt("Вася") )
+
+// Class 12
+// let str = prompt("Enter something", "ho-ho")
+// console.log(str);
+
+// Class 13
+// let str1 = +prompt('chyslo 1');
+// let str2 = +prompt('chyslo 2');
+// alert(str1+str2)
+
+// Class 14
+// let str1 = prompt('Name');
+// let str2 = prompt('Surname');
+// let str3 = prompt('age');
+// alert(`Доброго вечера ${str1} ${str2},  мои поздравления что вам ${str3}`)
